7c3fea846399de83442d9f75e002cc91a4284fec
Ed Wildgoose <ed+git@wildgooses.com>
2024-08-22 10:19:30 +0100

    Add benchee and reformat mix.exs

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule Sudoku.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :sudoku,
-     version: "1.1.0",
-     elixir: "~> 1.2",
-     description: "Sudoku solver in Elixir, which prefers to use heuristics over guessing",
-     package: package(),
-     source_url: "https://github.com/ewildgoose/elixir-sudoku",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :sudoku,
+      version: "1.1.0",
+      elixir: "~> 1.2",
+      description: "Sudoku solver in Elixir, which prefers to use heuristics over guessing",
+      package: package(),
+      source_url: "https://github.com/ewildgoose/elixir-sudoku",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -30,17 +32,19 @@ defmodule Sudoku.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-  [{:earmark, "~> 1.1", only: :dev},
-   {:ex_doc, "~> 0.14", only: :dev}]
+    [
+      {:earmark, "~> 1.1", only: :dev},
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:benchee, "~> 1.0", only: :dev}
+    ]
   end
 
   defp package do
     [
-     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
-     maintainers: ["Ed Wildgoose"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/ewildgoose/elixir-sudoku"}
-   ]
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Ed Wildgoose"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/ewildgoose/elixir-sudoku"}
+    ]
   end
-
 end
